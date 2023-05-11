2be683ecf7a0c11d889aa9c6e7a4085e251116af
Greg Walters <Greg@GregWWalters.dev>
2023-05-11 09:49:11 -0500

    Update tests, remove unnecessary defer

--- requestlogger_test.go
+++ requestlogger_test.go
@@ -10,36 +10,22 @@ import (
 	"time"
 )
 
-// MARK: Test Types
-
-// errContext is a context that immediately closes and returns an error that's
-// neither context.Canceled nor context.DeadlineExceeded
-type errContext struct {
-	done chan struct{}
-}
-
-func (ec errContext) Deadline() (deadline time.Time, ok bool) { return }
-func (ec errContext) Done() <-chan struct{} {
-	defer func() {
-		if ec.done != nil {
-			ec.done <- struct{}{}
-		}
-	}()
-	return ec.done
-}
-func (ec errContext) Err() error {
-	return errors.New("unusual context error")
-}
-func (ec errContext) Value(key interface{}) interface{} { return nil }
-
 // MARK: Re-used variables
-var mockHandler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-	time.Sleep(25 * time.Millisecond)
-	(rw).Header().Set("Content-Type", "text/plain")
-	(rw).WriteHeader(200)
-	_, _ = (rw).Write([]byte("done"))
-})
-var mockBody = []byte(`{ "bodyProperty": "bodyValue" }`)
+var (
+	mockHandler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		time.Sleep(25 * time.Millisecond)
+		(rw).Header().Set("Content-Type", "text/plain")
+		(rw).WriteHeader(200)
+		_, _ = (rw).Write([]byte("done"))
+	})
+	mockBody       = []byte(`{ "bodyProperty": "bodyValue" }`)
+	rr             = httptest.NewRecorder()
+	mockRequest, _ = http.NewRequest(
+		http.MethodGet,
+		"/test-path?testVar=testVal",
+		bytes.NewBuffer(mockBody))
+)
+
 var slowHandler = http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
 	res := make(chan struct{})
 	go func() {
@@ -51,208 +37,94 @@ var slowHandler = http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request
 	case <-rq.Context().Done():
 		(rw).Header().Set("Content-Type", "text/plain")
 		(rw).WriteHeader(500)
-		_, _ = (rw).Write([]byte("timeout"))
+		_, _ = (rw).Write([]byte("cancelled"))
 	case <-res:
 		(rw).Header().Set("Content-Type", "text/plain")
 		(rw).WriteHeader(200)
 		_, _ = (rw).Write([]byte("done"))
 	}
 })
-var cancelHandler = http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
-	res := make(chan struct{})
-	go func() {
-		time.Sleep(25 * time.Millisecond)
-		res <- struct{}{}
-	}()
 
-	select {
-	case <-rq.Context().Done():
-		(rw).Header().Set("Content-Type", "text/plain")
-		(rw).WriteHeader(500)
-		_, _ = (rw).Write([]byte("canceled"))
-	case <-res:
-		(rw).Header().Set("Content-Type", "text/plain")
-		(rw).WriteHeader(200)
-		_, _ = (rw).Write([]byte("done"))
-	}
+var errorHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	(w).Header().Set("Content-Type", "text/plain")
+	(w).WriteHeader(500)
+	_, _ = (w).Write([]byte("some error occurred"))
 })
+
+func TestMain(m *testing.M) {
+	// setup
+	mockRequest.Header.Set("Content-Type", "application/json")
+	// run
+	m.Run()
+	// teardown
+}
+func noExtras(t *testing.T) {
+	h := NewRequestLogger(RequestLoggerConfig{
+		Headers: false,
+		Params:  false,
+		Body:    false,
+	})
+	h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
+}
+func headersAndParams(t *testing.T) {
+	h := NewRequestLogger(RequestLoggerConfig{
+		Headers: true,
+		Params:  true,
+		Body:    false,
+	})
+	h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
+}
+func headersAndBody(t *testing.T) {
+	h := NewRequestLogger(RequestLoggerConfig{
+		Headers: true,
+		Params:  false,
+		Body:    true,
+	})
+	h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
+}
+func timedOutRequest(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 0)
+	defer cancel()
+	slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
+	h := NewRequestLogger(RequestLoggerConfig{})
+	h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
+}
+func cancelledRequest(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
+	h := NewRequestLogger(RequestLoggerConfig{})
+	h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
+}
+func handlerError(t *testing.T) {
+	ctx, cancel := context.WithCancelCause(context.Background())
+	defer cancel(errors.New("some other error"))
+	errorRequest, _ := http.NewRequestWithContext(ctx, "GET", "/error-op", nil)
+	h := NewRequestLogger(RequestLoggerConfig{})
+	h.Handle(errorHandler).ServeHTTP(rr, errorRequest)
+}
 
 func TestRequestLogger_Handle(t *testing.T) {
-	rr := httptest.NewRecorder()
-	var mockRequest, _ = http.NewRequest(
-		http.MethodGet,
-		"/test-path?testVar=testVal",
-		bytes.NewBuffer(mockBody))
-	mockRequest.Header.Set("Content-Type", "application/json")
 
 	t.Run("String Logger", func(t *testing.T) {
-		SetupLocalLogger(LogLevelTrace)
-
-		t.Run("no extras", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{})
-			h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-		})
-
-		t.Run("headers and params", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{
-				Headers: true,
-				Params:  true,
-				Body:    false,
-			})
-			h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-		})
-
-		t.Run("headers and body", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{
-				Headers: true,
-				Params:  false,
-				Body:    true,
-			})
-			h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-		})
-
-		t.Run("timed-out rq", func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), 0)
-			defer cancel()
-			slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-
-			h := NewRequestLogger(RequestLoggerConfig{})
-			h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
-		})
-
-		t.Run("cancelled rq", func(t *testing.T) {
-			ctx, cancel := context.WithCancel(context.Background())
-			cancel()
-			slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-
-			h := NewRequestLogger(RequestLoggerConfig{})
-			h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
-		})
-
-		t.Run("custom levels", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{
-				Tags:                  []string{"requests"},
-				NormalLevel:           LogLevelTrace,
-				DeadlineExceededLevel: LogLevelInfo,
-				ContextCancelledLevel: LogLevelInfo,
-				ContextErrorLevel:     LogLevelFatal,
-			})
-			t.Run("normal", func(t *testing.T) {
-				h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-			})
-			t.Run("timeout", func(t *testing.T) {
-				ctx, cancel := context.WithTimeout(context.Background(), 0)
-				defer cancel()
-				slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-				h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
-			})
-			t.Run("cancel", func(t *testing.T) {
-				ctx, cancel := context.WithCancel(context.Background())
-				cancel()
-				slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-				h.Handle(cancelHandler).ServeHTTP(rr, slowRequest)
-			})
-			t.Run("other error", func(t *testing.T) {
-				ctx := errContext{}
-				slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-				h.Handle(cancelHandler).ServeHTTP(rr, slowRequest)
-			})
-		})
+		SetupLocalLogger(LogLevelDebug)
+		t.Run("no extras", noExtras)
+		t.Run("headers and params", headersAndParams)
+		t.Run("headers and body", headersAndBody)
+		t.Run("timed-out rq", timedOutRequest)
+		t.Run("cancelled rq", cancelledRequest)
+		t.Run("handler error", handlerError)
 	})
 
 	t.Run("JSON Logger", func(t *testing.T) {
 		SetupCloudLogger(LogLevelDebug, []string{"logger", "test"})
 
-		t.Run("no extras", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{
-				Headers: false,
-				Params:  false,
-				Body:    false,
-			})
-			h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-		})
-
-		t.Run("headers and params", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{
-				Headers: true,
-				Params:  true,
-				Body:    false,
-			})
-			h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-		})
-
-		t.Run("headers and body", func(t *testing.T) {
-			h := NewRequestLogger(RequestLoggerConfig{
-				Headers: true,
-				Params:  false,
-				Body:    true,
-			})
-			h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
-		})
-
-		t.Run("timed-out rq", func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), 0)
-			defer cancel()
-			slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-			slowHandler := http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
-				res := make(chan struct{})
-				go func() {
-					time.Sleep(25 * time.Millisecond)
-					res <- struct{}{}
-				}()
-
-				select {
-				case <-rq.Context().Done():
-					(rw).Header().Set("Content-Type", "text/plain")
-					(rw).WriteHeader(500)
-					_, _ = (rw).Write([]byte("timeout"))
-				case <-res:
-					(rw).Header().Set("Content-Type", "text/plain")
-					(rw).WriteHeader(200)
-					_, _ = (rw).Write([]byte("done"))
-				}
-			})
-
-			h := NewRequestLogger(RequestLoggerConfig{})
-			h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
-		})
-
-		t.Run("cancelled rq", func(t *testing.T) {
-			ctx, cancel := context.WithCancel(context.Background())
-			cancel()
-			slowRequest, _ := http.NewRequestWithContext(ctx, "GET", "/slow-op", nil)
-			slowHandler := http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
-				res := make(chan struct{})
-				go func() {
-					time.Sleep(25 * time.Millisecond)
-					res <- struct{}{}
-				}()
-
-				select {
-				case <-rq.Context().Done():
-					(rw).Header().Set("Content-Type", "text/plain")
-					(rw).WriteHeader(500)
-					_, _ = (rw).Write([]byte("canceled"))
-				case <-res:
-					(rw).Header().Set("Content-Type", "text/plain")
-					(rw).WriteHeader(200)
-					_, _ = (rw).Write([]byte("done"))
-				}
-			})
-
-			h := NewRequestLogger(RequestLoggerConfig{})
-			h.Handle(slowHandler).ServeHTTP(rr, slowRequest)
-		})
-	})
-
-	t.Run("Sub-Logger", func(t *testing.T) {
-		SetupLocalLogger(LogLevelDebug)
-		sl := Sublogger("sub-logger")
-		h := NewRequestLogger(RequestLoggerConfig{
-			Logger: sl,
-			Tags:   []string{"requests"},
-		})
-		h.Handle(mockHandler).ServeHTTP(rr, mockRequest)
+		t.Run("no extras", noExtras)
+		t.Run("headers and params", headersAndParams)
+		t.Run("headers and body", headersAndBody)
+		t.Run("timed-out rq", timedOutRequest)
+		t.Run("cancelled rq", cancelledRequest)
+		t.Run("handler error", handlerError)
 	})
 
 	t.Run("Sub-Logger", func(t *testing.T) {
